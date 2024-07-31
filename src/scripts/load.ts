2763a33d3061c68c3b2d181a03e539c1a6940d53
joewuthrich <joerwuthrich@gmail.com>
2024-07-31 21:44:50 +1200

    Modified loading to be done with useEffect instead of events

--- src/scripts/load.ts
+++ src/scripts/load.ts
@@ -1,6 +1,4 @@
 export const load = () => {
-  window.addEventListener("load", () => {
-    document.getElementById("container").style.display = "";
-    document.getElementById("page-loader").style.display = "none";
-  });
+  document.getElementById("container").style.display = "";
+  document.getElementById("page-loader").style.display = "none";
 };
